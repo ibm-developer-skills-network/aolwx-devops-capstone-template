3a451b27699b4c6a51417281c6e166bef042fc5d
John Rofrano <johnnyroy@johnrofrano.com>
2022-10-07 21:29:54 +0000

    Fine tuning setup

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,21 +1,14 @@
-# DO NOT run this program as a bash script
-# It sets environment variable and so must be sourced
-#
-# Usage:
-#     source bin/setup
-#
-
+#!/bin/bash
 echo "****************************************"
 echo " Setting up Capstone Environment"
 echo "****************************************"
-export DEBIAN_FRONTEND=noninteractive
 
 echo "Updating package manager..."
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 
 echo "Installing Python 3.9 and Virtual Environment"
 sudo apt-get update
-sudo apt-get install -y python3.9 python3.9-venv
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3.9 python3.9-venv
 
 echo "Making Python 3.9 the default..."
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
@@ -26,22 +19,25 @@ python3 --version
 
 echo "Creating a Python virtual environment"
 python3 -m venv ~/venv
-echo ". ~/venv/bin/activate" >> ~/.bashrc
 
-echo "Changing the bash prompt..."
-export PS1="\[\e]0;\u:\W\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ "
+echo "Configuring the developer environment..."
+echo "# DevOps Capstone Project additions" >> ~/.bashrc
+echo ". ~/venv/bin/activate" >> ~/.bashrc
 echo 'export PS1="\[\e]0;\u:\W\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ "' >> ~/.bashrc
 
-echo "Activating virtual environment..."
-source ~/venv/bin/activate
-
 echo "Installing Python depenencies..."
-make install
+source ~/venv/bin/activate && python3 -m pip install --upgrade pip wheel
+source ~/venv/bin/activate && pip install -r requirements.txt
 
 echo "Starting the Postgres Docker container..."
 make db
 
+echo "Checking the Postgres Docker container..."
+docker ps
+
 echo "****************************************"
 echo " Capstone Environment Setup Complete"
 echo "****************************************"
+echo ""
+echo "Use 'exit' to close this terminal and open a new one to initialize the environment"
 echo ""
