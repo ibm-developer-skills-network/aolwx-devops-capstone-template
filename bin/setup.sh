784cbc029c41afa21a85e4a1444ab3ff9dd3ea83
John Rofrano <johnnyroy@johnrofrano.com>
2022-10-25 13:26:33 -0400

    Added GITHUB_ACCOUNT to .bashrc

--- bin/setup.sh
+++ bin/setup.sh
@@ -22,6 +22,7 @@ python3 -m venv ~/venv
 
 echo "Configuring the developer environment..."
 echo "# DevOps Capstone Project additions" >> ~/.bashrc
+echo "export GITHUB_ACCOUNT=$GITHUB_ACCOUNT" >> ~/.bashrc
 echo 'export PS1="\[\e]0;\u:\W\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ "' >> ~/.bashrc
 echo "source ~/venv/bin/activate" >> ~/.bashrc
 
