4a365274b9ae3eae20c6d732045ed13588ab46c9
Fran-Apps-Ds <franpe@apps-ds.com>
2023-12-04 23:12:58 +0100

    Added Talisman

--- service/__init__.py
+++ service/__init__.py
@@ -8,10 +8,12 @@ import sys
 from flask import Flask
 from service import config
 from service.common import log_handlers
+from flask_talisman import Talisman
 
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
+talisman = Talisman(app)
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
